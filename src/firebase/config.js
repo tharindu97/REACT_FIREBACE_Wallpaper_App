8618700c7b3cd5d745645d6968c64ead58e14b6e
tharindu97 <tharindukavishna@gmail.com>
2020-07-30 12:14:38 +0530

    firebase_setup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCN3nwNXbpNzlmdfqu8DuGt9OqEkPY02BY",
+    authDomain: "react-photo-cf165.firebaseapp.com",
+    databaseURL: "https://react-photo-cf165.firebaseio.com",
+    projectId: "react-photo-cf165",
+    storageBucket: "react-photo-cf165.appspot.com",
+    messagingSenderId: "838751079008",
+    appId: "1:838751079008:web:50de284bc4cfaf4d945105",
+    measurementId: "G-25C03F7PQE"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
