b5f8eb7f89937b15316770f8bd2f335ce6fa9623
tharindu97 <tharindukavishna@gmail.com>
2020-07-30 19:16:30 +0530

    photo_upload_completed

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,22 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCN3nwNXbpNzlmdfqu8DuGt9OqEkPY02BY",
-    authDomain: "react-photo-cf165.firebaseapp.com",
-    databaseURL: "https://react-photo-cf165.firebaseio.com",
-    projectId: "react-photo-cf165",
-    storageBucket: "react-photo-cf165.appspot.com",
-    messagingSenderId: "838751079008",
-    appId: "1:838751079008:web:50de284bc4cfaf4d945105",
-    measurementId: "G-25C03F7PQE"
-  };
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+  apiKey: "AIzaSyCN3nwNXbpNzlmdfqu8DuGt9OqEkPY02BY",
+  authDomain: "react-photo-cf165.firebaseapp.com",
+  databaseURL: "https://react-photo-cf165.firebaseio.com",
+  projectId: "react-photo-cf165",
+  storageBucket: "react-photo-cf165.appspot.com",
+  messagingSenderId: "838751079008",
+  appId: "1:838751079008:web:50de284bc4cfaf4d945105",
+  measurementId: "G-25C03F7PQE"
+};
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
